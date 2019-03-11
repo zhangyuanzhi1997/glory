6631c0e3f05516b530f35e0ad316127f38e7ba89
NikoManiac <niko.sky.lan@gmail.com>
2019-03-11 23:16:51 +0800

    project init

---
+++ src/main/java/glory/GloryApplication.java
@@ -0,0 +1,13 @@
+package glory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GloryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/glory/GloryApplicationTests.java
@@ -0,0 +1,16 @@
+package glory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GloryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
